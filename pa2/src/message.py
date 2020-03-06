9cb1c220535bde5190085968fa0cae7aafb82b51
arjun-rao <mailarjunrao@gmail.com>
2020-03-06 12:04:56 -0700

    wip: multicast

--- pa2/src/message.py
+++ pa2/src/message.py
@@ -1,10 +1,25 @@
 # message class
+import json
+
 class Message:
     def __init__(self,mid,data,sender,gsid=None):
         self.gs_id = gsid
         self.m_id = mid
         self.data = data
         self.sender = sender
+
+    def to_string(self):
+        return json.dumps({
+            'mid': self.m_id,
+            'gsid': self.gs_id,
+            'sender': self.sender,
+            'data': self.data
+        })
+
+    @classmethod
+    def from_string(cls, data):
+        payload = json.loads(data)
+        return Message(payload['mid'], payload['data'], payload['sender'], payload['gsid'])
 
     def has_gsid(self):
         if self.gs_id == None:
--- pa2/src/server.py
+++ pa2/src/server.py
@@ -6,6 +6,12 @@ from ftqueue import FTQueue
 from logger import get_logger
 import multiprocessing
 from message import Message
+import struct
+
+MCAST_GRP = '228.8.8.8'
+MCAST_PORT = 5007
+MCAST_IFACE = '127.0.0.1'
+MULTICAST_TTL = 1
 
 class Server:
     def __init__(self, sid, address, logfile='server.log'):
@@ -35,6 +41,7 @@ class UDPServer(Server):
             PORT, int: The UDP port to listen on.
         """
         self.client_socket = None
+        self.multicast_socket = None
         self.HOST = HOST
         self.PORT = PORT
         self._id = sid
@@ -50,24 +57,41 @@ class UDPServer(Server):
         # Server's to_process queue holds messages from clients
         self.manager = multiprocessing.Manager()
         self.to_process = self.manager.list()
+        self.multicast_buffer = self.manager.list()
 
         self._address = '{}:{}'.format(self.HOST, self.PORT)
         super(UDPServer, self).__init__(self._id, self._address)
         self._bind()
+
+        # Capture control-C signals
         def handler(signal_received, frame):
             # Handle any cleanup here
             print('SIGINT or CTRL-C detected. Exiting gracefully')
             if self.client_socket is not None:
                 self.client_socket.close()
+            if self.multicast_socket is not None:
+                self.multicast_socket.close()
             exit(0)
         signal(SIGINT, handler)
 
     def _bind(self):
-        """Binds a UDP socket to the configured hostname and port
+        """Binds a UDP sockets to the configured hostname and port
         """
         try:
             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
             self.client_socket.bind((self.HOST, self.PORT))
+        except Exception as e:
+            self.log_exception('Failed to bind socket to host')
+        # Multicast socket
+        try:
+            self.multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+            self.multicast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.multicast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+            self.multicast_socket.bind((MCAST_GRP, MCAST_PORT))
+            mreq = struct.pack('4s4s', socket.inet_aton(MCAST_GRP), socket.inet_aton(MCAST_IFACE))
+            self.multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+            self.multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
         except Exception as e:
             self.log_exception('Failed to bind socket to host')
 
@@ -125,10 +149,48 @@ class UDPServer(Server):
                 mid = int(f'{self._id}0{self.counter}')
                 msg = Message(mid, data, addr)
                 # Multicast
-                # TODO: Multicast here
+                msg_str = msg.to_string()
+                self.multicast_socket.sendto(str.encode(msg_str), (MCAST_GRP, MCAST_PORT))
                 # Add to buffer
                 print('MID:', msg.m_id)
                 self.to_process.append(msg)
+
+            except Exception:
+                self.log_exception('An error occurred while listening for messages...')
+
+    def handle_multicast_recv(self):
+        """Reads messages from muticast port, and process:
+            1)Stores in buffer
+            1.1) Check if message recieved has a GSID
+            If GSID: go to 2.2.1
+            If no GSID:
+            2) Checks if it is the responsible server by message_id % n
+                2.1)If it is responsible server:
+                    1. Assigns a GSID to the message
+                    2. Multicasts <message_id, message, GSID>
+                    3. Updates GSID
+                    4. Deliver/process message
+  2.2)Else:
+                    1. On recieving global sequence ID:
+1. Checks if GSID received matches with expected GSID:
+                            If True: Updates GSID, removes message from buffer, checks  for other recieved messages to process
+                            Else:
+                                Check if missing GSID in toDeliver Buffer
+                                If not in buffer: Adds message with GSID to toDeliver buffer,
+                                    contacts responsible server for missed message
+                                    On recieving missed message, go to 2.1.1
+                                If in buffer: go to 2.2.1.1
+        """
+        self.log_info('Listening for messages...')
+        while True:
+            try:
+                data, addr = self.multicast_socket.recvfrom(self.BUFFERSIZE)
+                self.log_info('Received msg: {} from: {}'.format(data, addr))
+                data = data.decode()
+                msg = Message.from_string(data)
+                # Step 1: Add to buffer
+                print('Multicast MID:', msg.m_id)
+                self.multicast_buffer.append(msg)
 
             except Exception:
                 self.log_exception('An error occurred while listening for messages...')
@@ -140,11 +202,59 @@ class UDPServer(Server):
         process.daemon = True
         process.start()
         # Check if there is a message to process:
+        process_buffer = False
+        multicast_flag = False
         while True:
-            if self.to_process:
+            if self.to_process and not process_buffer:
                 # There is a message in the to_process buffer.
                 # Check if message ID % n + 1 == self.GSID
-                pass
+                print('Client Recv {}', self.to_process[0].mid)
+                to_process= True
+            if self.multicast_buffer and not multicast_flag:
+                # There is a message in the to_process buffer.
+                # Check if message ID % n + 1 == self.GSID
+                print('Multicast Recv {}', self.multicast_buffer[0].mid)
+                multicast_flag= True
+                # Check if message ID % n + 1 == self.GSID
+                print('Client Recv {}', self.to_process[0].mid)
+                to_process= True
+            if self.multicast_buffer and not multicast_flag:
+                # There is a message in the to_process buffer.
+                # Check if message ID % n + 1 == self.GSID
+                print('Multicast Recv {}', self.multicast_buffer[0].mid)
+                multicast_flag= True
+                # Check if message ID % n + 1 == self.GSID
+                print('Client Recv {}', self.to_process[0].mid)
+                to_process= True
+            if self.multicast_buffer and not multicast_flag:
+                # There is a message in the to_process buffer.
+                # Check if message ID % n + 1 == self.GSID
+                print('Multicast Recv {}', self.multicast_buffer[0].mid)
+                multicast_flag= True
+                # Check if message ID % n + 1 == self.GSID
+                print('Client Recv {}', self.to_process[0].mid)
+                to_process= True
+            if self.multicast_buffer and not multicast_flag:
+                # There is a message in the to_process buffer.
+                # Check if message ID % n + 1 == self.GSID
+                print('Multicast Recv {}', self.multicast_buffer[0].mid)
+                multicast_flag= True
+                # Check if message ID % n + 1 == self.GSID
+                print('Client Recv {}', self.to_process[0].mid)
+                to_process= True
+            if self.multicast_buffer and not multicast_flag:
+                # There is a message in the to_process buffer.
+                # Check if message ID % n + 1 == self.GSID
+                print('Multicast Recv {}', self.multicast_buffer[0].mid)
+                multicast_flag= True
+                # Check if message ID % n + 1 == self.GSID
+                print('Client Recv {}', self.to_process[0].mid)
+                to_process= True
+            if self.multicast_buffer and not multicast_flag:
+                # There is a message in the to_process buffer.
+                # Check if message ID % n + 1 == self.GSID
+                print('Multicast Recv {}', self.multicast_buffer[0].mid)
+                multicast_flag= True
 
 
 
